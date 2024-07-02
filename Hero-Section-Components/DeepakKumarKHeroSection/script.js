a35c2ff1deef0e1ac1bbb6e681d9fb91a2733da2
Deepak Kumar <deepakdhoni7781@gmail.com>
2024-07-02 13:15:25 +0530

    Added Hero-Section Template by Deepak Kumar K

--- Hero-Section-Components/DeepakKumarKHeroSection/script.js
+++
@@ -1,4 +0,0 @@
-document.querySelector('.btn').addEventListener('click', function(e) {
-    e.preventDefault();
-    document.querySelector('#get-started').scrollIntoView({ behavior: 'smooth' });
-});
